6bf0bc95fb40cfa33a445b6733b5565c108e1477
CJ Andren <admin@quicklearn.online>
2021-10-17 18:51:42 -0400

    testing update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router,
+        Switch,
+        Route,
+        Link
+
+} from "react-router-dom";
 
 function App() {
   return (
@@ -7,7 +14,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Testing the systems for launch!
+            Testing the systems for launch!
+        </p>
+
+        <p>
+            Information is power!
         </p>
       </header>
     </div>
