4f7a855fa2a43988f306e1d02aa9a3d71a4eba25
CJ Andren <admin@quicklearn.online>
2021-10-24 22:00:49 -0400

    added STL support

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import STLViewer from 'stl-viewer';
 import { BrowserRouter as Router,
         Switch,
         Route,
@@ -13,6 +14,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <STLViewer
+            url='https://quicklearn.online/models/monket-test.stl'
+            width={400}
+            height={400}
+            rotate={true}
+        />
         <p>
             Testing the systems for launch!
         </p>
